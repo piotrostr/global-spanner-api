7103b1a7ae681ac8d8ec2a1db21b77cc4f2dc4da
Piotr Ostrowski <piotrostr@google.com>
2022-11-16 21:15:31 +0000

    use spannertest mock server for tests

--- main_test.go
+++ main_test.go
@@ -1,18 +1,42 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
+
+	"cloud.google.com/go/spanner/spannertest"
+	"github.com/gin-gonic/gin"
 )
 
-func TestHealthCheckWorks(t *testing.T) {
-	client, err := SetupClient()
-	if err != nil {
-		t.Errorf("Error setting up client: %v", err)
-	}
-	router := SetupRouter(client)
+var client *Client
 
+var router *gin.Engine
+
+func TestMain(m *testing.M) {
+	srv, err := spannertest.NewServer("localhost:0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer srv.Close()
+
+	os.Setenv("SPANNER_EMULATOR_HOST", srv.Addr)
+
+	client, err = SetupClient()
+	if err != nil {
+		log.Fatalf("Error setting up client: %v", err)
+	}
+	router = SetupRouter(client)
+
+	code := m.Run()
+
+	client.Teardown()
+	os.Exit(code)
+}
+
+func TestHealthCheckWorks(t *testing.T) {
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/", nil)
 	router.ServeHTTP(w, req)
@@ -27,12 +51,6 @@ func TestHealthCheckWorks(t *testing.T) {
 }
 
 func TestAddNamesWorks(t *testing.T) {
-	client, err := SetupClient()
-	if err != nil {
-		t.Errorf("Error setting up client: %v", err)
-	}
-	router := SetupRouter(client)
-
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("POST", "/add-names", nil)
 	router.ServeHTTP(w, req)
@@ -47,12 +65,6 @@ func TestAddNamesWorks(t *testing.T) {
 }
 
 func TestGetNamesWorks(t *testing.T) {
-	client, err := SetupClient()
-	if err != nil {
-		t.Errorf("Error setting up client: %v", err)
-	}
-	router := SetupRouter(client)
-
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/get-names", nil)
 	router.ServeHTTP(w, req)
